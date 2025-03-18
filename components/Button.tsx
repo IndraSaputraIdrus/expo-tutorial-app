7e0ff5c1500bae4170b74a4b39749334af4b6100
indra saputra idrus <indrasaputraidrus@gmail.com>
2025-03-18 22:15:23 +0800

    chapter 4: use an image picker

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,9 +4,10 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 type Props = {
   label: string;
   theme?: "primary";
+  onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === "primary") {
     return (
       <View
@@ -15,11 +16,14 @@ export default function Button({ label, theme }: Props) {
           { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
         ]}
       >
-        <Pressable 
-          onPress={() => alert("You pressed a button")}
-          style={[styles.button, { backgroundColor: "#fff" }]}>
+        <Pressable
+          onPress={onPress}
+          style={[styles.button, { backgroundColor: "#fff" }]}
+        >
           <FontAwesome name="picture-o" style={styles.buttonIcon} size={18} />
-          <Text style={[styles.buttonLabel, {color: "#25292e"}]}>{label}</Text>
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
         </Pressable>
       </View>
     );
@@ -27,7 +31,7 @@ export default function Button({ label, theme }: Props) {
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button}>
+      <Pressable onPress={onPress} style={styles.button}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
@@ -57,5 +61,5 @@ const styles = StyleSheet.create({
   },
   buttonIcon: {
     paddingRight: 8,
-  }
+  },
 });
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,10 +3,13 @@ import { StyleSheet } from "react-native";
 
 type Props = {
   imageSource: ImageSource;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({ imageSource }: Props) {
-  return <Image source={imageSource} style={styles.image} />;
+export default function ImageViewer({ imageSource, selectedImage }: Props) {
+  const imgSource = selectedImage ? { uri: selectedImage } : imageSource;
+
+  return <Image source={imgSource} style={styles.image} />;
 }
 
 const styles = {
